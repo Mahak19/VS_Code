be16c6f845c4de86b92d333a06a6f619113a3897
Mahak19 <142742988+Mahak19@users.noreply.github.com>
2023-10-07 13:14:20 +0530

    Add files via upload

---
+++ Extra/script.js
@@ -0,0 +1,19 @@
+/*<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js"
+    integrity="sha512-pumBsjNRGGqkPzKHndZMaAG+bir374sORyzM3uulLV14lN5LyykqNk8eEeUlUkB3U0M4FApyaHraT65ihJhDpQ=="
+    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    $(function () {
+      $("#header").load("header.html");
+     
+
+    }) 
+    <script>"https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"</script>
+    <script>
+    $(function() {
+      $('#header').load('header.html')
+    })
+    </script>*/
+  function myfun(){
+    let a=1;
+    let b=2;
+    console.log(a+b);
+  }
